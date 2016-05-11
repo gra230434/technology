360e0aac573022cecfca89ea739c9c2da9d84757
Kevin W <gra230434@gmail.com>
2016-05-12 01:09:06 +0800

    command fix

--- comments.php
+++ comments.php
@@ -1,47 +1,45 @@
 <?php if(comments_open()) : ?>
 
-<?php if($comments) : ?>
-	<h3>大家是怎麼想的呢:</h3>
-	<ol>
-  	<?php foreach($comments as $comment) : ?>
-		<li>
-			<?php if($comment->comment_approved == '0') : ?>
-				<p>你的留言還未經過管理員審核</p>
-				<p>如果想盡快通過審核，可以直接寫信通知管理人員</p>
-				<p>聯絡信箱: gra230434@gmail.com</p>
-			<?php else : ?>
-			<h4><?php comment_type(); ?> by <?php comment_author_link(); ?> on <?php comment_date(); ?></h4>
-			<?php comment_text(); ?>
-			<?php endif; ?>
-			<div class="line"></div>
-		</li>
-	<?php endforeach; ?>
-	</ol>
+<?php if( have_comments() ) : ?>
+	<h3 class="comments-title">大家是怎麼想的呢:</h3>
+
+	<?php the_comments_navigation(); ?>
+    <ol class="comment-list">
+      <?php  wp_list_comments( array(  'style'       => 'ol',
+                                       'short_ping'  => true,
+                                       'avatar_size' => 42,  ) ); ?>
+    </ol><!-- .comment-list -->
+  <?php the_comments_navigation(); ?>
+
 <?php else : ?>
 	<h3>目前還沒有留言  留言是我努力的最大動力</h3>
 <?php endif; ?>
 
-	<?php if(get_option('comment_registration') && !$user_ID) : ?>
-		<p>You must be <a href="<?php echo site_url(''); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p>
-	<?php else : ?>
-		<form action="<?php echo site_url(''); ?>/wp-comments-post.php" method="post" id="commentform">
-			<?php if($user_ID) : ?>
-				<p>以<a href="<?php echo site_url(''); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>登入 <a href="<?php echo site_url(''); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a></p>
-			<?php else : ?>
-				<h3>來留下你的想法吧!</h3>
-				<p><input type="text" name="author" id="author" placeholder="your name"  value="<?php echo $comment_author; ?>" size="22" tabindex="1" />
-				<label for="author"><small>Name <?php if($req) echo "(必填)"; ?></small></label></p>
-				<p><input type="email" name="email" id="email" placeholder="your Email address"  value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
-				<label for="email"><small>Mail</small></label></p>
-				<p><input type="url" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-				<label for="url"><small>個人網站</small></label></p>
-			<?php endif; ?>
-			<p><textarea name="comment" id="comment" style="width:100%;height:82px;font-size:16px;"></textarea></p>
-			<p><input name="submit" type="submit" id="submit" tabindex="5" value="Post comment" />
-			<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" /></p>
-			<?php do_action('comment_form', $post->ID); ?>
-		</form>
-	<?php endif; ?>
+	<?php
+	  $comment_firlds_args = array(
+			'author' =>
+        '<p class="comment-form-author"><input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .'" size="30"/>' .
+				'<label for="author">Name</label><span class="required">*</span></p>',
+
+      'email' =>
+        '<p class="comment-form-email"><input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"/>' .
+		    '<label for="email">Email</label><span class="required">*</span></p>',
+
+      'url' =>
+        '<p class="comment-form-url"><input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+		    '" size="30" /><label for="url">Website</label></p>',
+		);
+	  $comment_args = array(
+			'title_reply'=>'來留下你的想法吧!',
+			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+			'title_reply_after'  => '</h2>',
+			'fields' => apply_filters( 'comment_form_default_fields', $comment_firlds_args ),
+			'comment_field' => '<p><textarea name="comment" id="comment" style="width:100%;height:82px;font-size:16px;"></textarea></p>',
+			'comment_notes_after' => '',
+		);
+		comment_form( $comment_args );
+	?>
+	
 <?php else : ?>
 	<p>這篇的留言是被關閉的</p>
 <?php endif; ?>
--- functions.php
+++ functions.php
@@ -8,11 +8,12 @@
 /*sidebar*/
    if ( function_exists('register_sidebar') )
           register_sidebar(array(
-          'name' => 'SideBar',
+          'name'          => 'SideBar',
+          'id'            => 'SideBar-left',
           'before_widget' => '<div class="sidebar">',
-          'after_widget' => '</div>',
-          'before_title' => '<h3>',
-          'after_title' => '</h3>',
+          'after_widget'  => '</div>',
+          'before_title'  => '<h3>',
+          'after_title'   => '</h3>',
           ));
 
 /*read more*/
@@ -28,17 +29,28 @@ function wp_pagenavi() {
 	global $wp_query;
 	$max = $wp_query -> max_num_pages;
 	if (!$current = get_query_var('paged')) $current=1;
-	
+
 	$args['base'] = str_replace(999999999, '%#%', get_pagenum_link(999999999));
 	$args['total'] = $max;
 	$args['current'] = $current;
 	$args['prev_text'] = '<';
 	$args['next_text'] = '>';
-	
-	if ( $max > 1 ){ $pages = '<div 
-	class="wp-pagenavi"><span class="pages">共 ' . $max . ' 頁</span>';
+
+	if ( $max > 1 ){ $pages = '<div class="wp-pagenavi"><span class="pages">共 ' . $max . ' 頁</span>';
 	echo $pages . paginate_links($args) . '</div>';
-	}	
+	}
 }
+/*
+ * 改變 comment note above comment author
+ *
+ * @since technology 1.1
+*/
+function tech_move_comment_field_to_bottom( $fields ) {
+  $comment_field = $fields['comment'];
+  unset( $fields['comment'] );
+  $fields['comment'] = $comment_field;
+  return $fields;
+}
+add_filter( 'comment_form_fields', 'tech_move_comment_field_to_bottom' );
 
 ?>
